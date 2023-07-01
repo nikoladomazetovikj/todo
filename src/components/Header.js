1f9a8d1a4ac79072394135e6cdb7160cf12ed775
nikoladomazetovikj <nikolad150@gmail.com>
2023-07-01 11:45:37 +0200

    feat: header and background style

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import '../styles/Header.css';
+
+function Header() {
+    return (
+        <div>
+            <h1 className='text-center margin-top-header title'>Todo List</h1>
+        </div>
+    );
+}
+
+
+export default Header;
