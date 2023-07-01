05a89bb8578fe03f405aa956af6cde182541d51f
nikoladomazetovikj <nikolad150@gmail.com>
2023-07-01 15:14:39 +0200

    fix: on select change border color

--- src/components/MyList.js
+++ src/components/MyList.js
@@ -1,20 +1,23 @@
-import '../styles/ListItems.css';
 import Button from "../UI/Button";
+import '../styles/ListItems.css'
 
-function MyList({items, onDelete, onUpdate}) {
+function MyList({ items, onDelete, onUpdate }) {
     return (
         <ul className="notebook-list">
             {items.map((item) => (
-                <li key={item.id}>
-                    <input type="checkbox" id={item.id} onChange={() => onUpdate(item.id)}/>
+                <li
+                    key={item.id}
+                    className={`notebook-list ${item.is_done ? 'green' : 'red'}`}
+                >
+                    <input type="checkbox" id={item.id} onChange={() => onUpdate(item.id)} />
                     <label htmlFor={item.id}>{item.item}</label>
-                    <Button className="delete-button" onClick={() => onDelete(item.id)}>X</Button>
+                    <Button className="delete-button" onClick={() => onDelete(item.id)}>
+                        X
+                    </Button>
                 </li>
             ))}
         </ul>
-
     );
 }
-
 
 export default MyList;
