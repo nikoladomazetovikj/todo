6d18b555862750cab1ae2154d3fee4a479ece409
nikoladomazetovikj <nikolad150@gmail.com>
2023-07-01 12:47:07 +0200

    feat: list items

--- src/components/CardsContainer.js
+++ src/components/CardsContainer.js
@@ -1,9 +1,15 @@
 import AddElement from "./AddElement";
+import MyList from "./MyList";
 
 function CardsContainer () {
-    return <div className='centered-position margin-top'>
-        <AddElement/>
-    </div>
+    return <>
+        <div className='centered-position margin-top'>
+            <AddElement/>
+        </div>
+        <div className='centered-position margin-top'>
+            <MyList/>
+        </div>
+    </>
 }
 
 export default CardsContainer;
--- src/components/MyList.js
+++ src/components/MyList.js
@@ -1,4 +1,27 @@
-function MyList() {}
+import '../styles/ListItems.css';
+
+function MyList() {
+    return (
+        <ul className="notebook-list">
+            <li>
+                <input type="checkbox" id="item1"/>
+                <label htmlFor="item1">List item 1</label>
+                <button className="delete-button">X</button>
+            </li>
+            <li>
+                <input type="checkbox" id="item2"/>
+                <label htmlFor="item2">List item 2</label>
+                <button className="delete-button">X</button>
+            </li>
+            <li>
+                <input type="checkbox" id="item3"/>
+                <label htmlFor="item3">List item 3</label>
+                <button className="delete-button">X</button>
+            </li>
+        </ul>
+
+    );
+}
 
 
 export default MyList;
